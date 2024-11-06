7d992cb22b977b5a0f8ab8852e820d2ffe48bdd8
sobarine21 <sobarinefinance@gmail.com>
2024-11-07 01:23:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,7 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.primitives import padding
-from cryptography.hazmat.primitives import rsa
 import base64
-from PIL import Image
 import qrcode
 import zipfile
 import io
@@ -127,4 +124,3 @@ elif option == "Decrypt":
             decrypted_pdf = decrypt_pdf_aes(uploaded_file, decryption_key)
             st.write("Decrypted PDF content:")
             st.text(decrypted_pdf.decode('utf-8'))
-
